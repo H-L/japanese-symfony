4290f864d3df96ec280a91f476964381c06f3a9e
m-a-l <m-a-les@hotmail.fr>
2017-02-14 23:43:27 +0100

    :sparkles: [Users] FOSusers configured, branch dev init

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,6 +8,7 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
+            new FOS\UserBundle\FOSUserBundle(),
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
