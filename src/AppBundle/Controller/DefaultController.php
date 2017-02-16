08f97cf0e229a191376e0eee81c5894ab989cf1e
H-L <hadrien.lepoutre@gmail.com>
2017-02-16 02:16:07 +0100

    :sparkles: Adding Image & Galleries Entities with simple views for debug

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,21 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ));
     }
+
+    /**
+     * @Route("/galleries", name="galleries")
+     */
+    public function testingImageGalleriesAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $gallery = $em->getRepository('AppBundle:Gallery')->find(1);
+        $images = $gallery->getImages()->toArray();
+
+        // replace this example code with whatever you need
+        return $this->render('galleries/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'images' => $images,
+            'gallery' => $gallery,
+        ));
+    }
 }
---
+++ src/AppBundle/Entity/Image.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Image
+ *
+ * @ORM\Table(name="images")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ImageRepository")
+ */
+class Image
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="path", type="string", length=255)
+     */
+    private $path;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Image
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     *
+     * @return Image
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Gallery", mappedBy="images")
+     */
+    private $galleries;
+
+    public function __construct() {
+        $this->galleries = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add gallery
+     *
+     * @param \AppBundle\Entity\Gallery $gallery
+     *
+     * @return Image
+     */
+    public function addGallery(\AppBundle\Entity\Gallery $gallery)
+    {
+        $this->galleries[] = $gallery;
+
+        return $this;
+    }
+
+    /**
+     * Remove gallery
+     *
+     * @param \AppBundle\Entity\Gallery $gallery
+     */
+    public function removeGallery(\AppBundle\Entity\Gallery $gallery)
+    {
+        $this->galleries->removeElement($gallery);
+    }
+
+    /**
+     * Get galleries
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getGalleries()
+    {
+        return $this->galleries;
+    }
+}
