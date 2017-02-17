a4d0bc87cc3b3d84fb6718cee2c9a946be60dfe5
m-a-l <m-a-les@hotmail.fr>
2017-02-17 10:41:33 +0100

    :art: :lipstick: initialised all front - gulpfile, scss, minify, bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp         = require('gulp'),
+    concat       = require('gulp-concat'),
+    sass         = require('gulp-sass'),
+    postcss      = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    uglify       = require('gulp-uglify'),
+    pump         = require('pump'),
+    cleanCSS = require('gulp-clean-css'),
+    exec = require('child_process').exec;
+
+var processors = [
+  autoprefixer
+];
+
+gulp.task('scss', function() {
+    return gulp.src('app/Resources/scss/main.scss')
+        .pipe(sass())
+        .pipe(postcss(processors))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('web/css'))
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('app/Resources/js/**')
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('web/js'));
+});
+
+gulp.task('server', function () {
+    exec('php app/console server:run', logStdOutAndErr);
+});
+
+// Without this function exec() will not show any output
+var logStdOutAndErr = function (err, stdout, stderr) {
+    console.log(stdout + stderr);
+};
+
+gulp.task('sync', ['server','scss', 'scripts'], function() {
+    gulp.watch("app/Resources/scss/**/*.scss", ['scss']);
+    gulp.watch("app/Resources/js/**", ['scripts']);
+});
